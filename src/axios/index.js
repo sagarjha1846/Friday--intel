283c8d753a4e3ce32c0b71e2bc341f33f6524496
Sagar Jha <sagarj.ext@shapoorji.com>
2023-03-13 18:31:10 +0530

    adding new api's to making all call in the same time

--- src/axios/index.js
+++ src/axios/index.js
@@ -1,8 +1,36 @@
 import axios from "axios";
+import constants from '../constant/routesConstant';
+
+const { backendURL } = constants;
+
+const URLs = [
+  { route: 'darkweb.php', name: 'torDomain', key: 'domain' },
+  { route: 'domain.php', name: 'domain', key: 'domain' },
+  { route: 'urls.php', name: 'url', key: 'domain' },
+  { route: 'files.php', name: 'files', key: 'hash' },
+  { route: 'ips.php', name: 'IP', key: 'ip' },
+];
+
+const getParams = (key, searchQuery) => {
+  return { [key]: searchQuery };
+};
+const fetchData = async (endpoint, searchQuery, key) => {
+  console.log(getParams(key, searchQuery));
+  const response = await axios.get(`${backendURL}${endpoint}`, {
+    params: getParams(key, searchQuery),
+  });
+
+  const data = await response?.data;
+
+  return {
+    name: URLs.filter((el) => el.route === endpoint)[0].name,
+    list: data,
+  };
+};
 
 const getTORDomain = async (searchRef) => {
   try {
-    const res = await axios.get("https://fridayintel.io/api-dev/darkweb.php", {
+    const res = await axios.get(backendURL + 'darkweb.php', {
       params: {
         domain: searchRef,
       },
@@ -15,7 +43,7 @@ const getTORDomain = async (searchRef) => {
 
 const getDomain = async (searchRef) => {
   try {
-    const res = await axios.get("https://fridayintel.io/api-dev/domain.php", {
+    const res = await axios.get(backendURL + 'domain.php', {
       params: { domain: searchRef },
     });
     return res;
@@ -26,7 +54,7 @@ const getDomain = async (searchRef) => {
 
 const getUrl = async (searchRef) => {
   try {
-    const res = await axios.get("https://fridayintel.io/api-dev/urls.php", {
+    const res = await axios.get(backendURL + 'urls.php', {
       params: { domain: searchRef },
     });
     return res;
@@ -37,7 +65,7 @@ const getUrl = async (searchRef) => {
 
 const getFiles = async (searchRef) => {
   try {
-    const res = await axios.get("https://fridayintel.io/api-dev/files.php", {
+    const res = await axios.get(backendURL + 'files.php', {
       params: { hash: searchRef },
     });
     return res;
@@ -48,7 +76,7 @@ const getFiles = async (searchRef) => {
 
 const getIP = async (searchRef) => {
   try {
-    const res = await axios.get("https://fridayintel.io/api-dev/ips.php", {
+    const res = await axios.get(backendURL + 'ips.php', {
       params: { ip: searchRef },
     });
     return res;
@@ -57,4 +85,4 @@ const getIP = async (searchRef) => {
   }
 };
 
-export { getTORDomain, getDomain, getUrl, getFiles, getIP };
+export { getTORDomain, getDomain, getUrl, getFiles, getIP, fetchData, URLs };
--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,29 +1,32 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import {
   Sidebar,
   Menu,
   MenuItem,
   SubMenu,
   useProSidebar,
-} from "react-pro-sidebar";
-import terroristprofilling from "../images/svg/terrorist.svg";
-import network from "../images/svg/network.svg";
-import "../css/sidebar.css"
-import React from "react";
-import { getDomain, getFiles, getTORDomain, getUrl, getIP } from "../axios";
-import { useState } from "react";
+} from 'react-pro-sidebar';
+import terroristprofilling from '../images/svg/terrorist.svg';
+import network from '../images/svg/network.svg';
+import '../css/sidebar.css';
+import React from 'react';
+import { getDomain, getFiles, getTORDomain, getUrl, getIP } from '../axios';
+import { useState } from 'react';
 // import { FiChevronsLeft, FiChevronsRight } from "react-icons/fi/";
-import { BsGlobe2} from "react-icons/bs";
-import { VscFile } from "react-icons/vsc";
-import { IoDocumentLockOutline } from "react-icons/io5";
-import {BiMessageRoundedDots} from "react-icons/bi";
-import { GrGroup } from "react-icons/gr";
-import {MdOutlineKeyboardArrowLeft, MdOutlineKeyboardArrowRight} from "react-icons/md";
+import { BsGlobe2 } from 'react-icons/bs';
+import { VscFile } from 'react-icons/vsc';
+import { IoDocumentLockOutline } from 'react-icons/io5';
+import { BiMessageRoundedDots } from 'react-icons/bi';
+import { GrGroup } from 'react-icons/gr';
+import {
+  MdOutlineKeyboardArrowLeft,
+  MdOutlineKeyboardArrowRight,
+} from 'react-icons/md';
 
 const Sidenav = (props) => {
   const { setNodeInfo, searchRef } = props;
   const { collapseSidebar } = useProSidebar();
   const [collapsed, setCollapsed] = useState(false);
-
   const [toggled, setToggled] = useState(false);
 
   const handleCollapsedChange = () => {
@@ -37,7 +40,7 @@ const Sidenav = (props) => {
     setNodeInfo({ list: [] });
     const response = await getTORDomain(searchRef);
     if (response?.data) {
-      setNodeInfo({ list: [...response.data], name: "torDomain" });
+      setNodeInfo({ list: [...response.data], name: 'torDomain' });
       collapseSidebar();
     }
   };
@@ -45,7 +48,7 @@ const Sidenav = (props) => {
     setNodeInfo({ list: [] });
     const response = await getDomain(searchRef);
     if (response?.data) {
-      setNodeInfo({ list: [...response.data], name: "domain" });
+      setNodeInfo({ list: [...response.data], name: 'domain' });
       collapseSidebar();
     }
   };
@@ -54,17 +57,19 @@ const Sidenav = (props) => {
     setNodeInfo({ list: [] });
     const response = await getUrl(searchRef);
     if (response?.data) {
-      setNodeInfo({ list: [...response.data], name: "url" });
+      setNodeInfo({ list: [...response.data], name: 'url' });
       collapseSidebar();
     }
     // else{
     //   setNodeInfo("result not found");
     // collapseSidebar();
     // }
-    if(response.data.Result === "Not Found"){
-      setNodeInfo({list:[...response.data.Result], name:"Result not found"});
+    if (response.data.Result === 'Not Found') {
+      setNodeInfo({
+        list: [...response.data.Result],
+        name: 'Result not found',
+      });
       collapseSidebar();
-
     }
   };
 
@@ -72,7 +77,7 @@ const Sidenav = (props) => {
     setNodeInfo({ list: [] });
     const response = await getIP(searchRef);
     if (response?.data) {
-      setNodeInfo({ list: [...response.data], name: "IP" });
+      setNodeInfo({ list: [...response.data], name: 'IP' });
       collapseSidebar();
     }
   };
@@ -81,58 +86,55 @@ const Sidenav = (props) => {
     setNodeInfo({ list: [] });
     const response = await getFiles(searchRef);
     if (response?.data) {
-      setNodeInfo({ list: [...response.data], name: "files" });
+      setNodeInfo({ list: [...response.data], name: 'files' });
       collapseSidebar();
     }
   };
 
   return (
     <Sidebar
-  className="sidebar"
-        style={{ height: "100vh", position: "absolute" }}
-        collapsed={collapsed}
-        toggled={toggled}
-        handleToggleSidebar={handleToggleSidebar}
-        handleCollapsedChange={handleCollapsedChange}
-    >
+      className="sidebar"
+      style={{ height: '100vh', position: 'absolute' }}
+      collapsed={collapsed}
+      toggled={toggled}
+      handleToggleSidebar={handleToggleSidebar}
+      handleCollapsedChange={handleCollapsedChange}>
       <main>
-    
-          <Menu onClick={() => collapseSidebar()} className="collapse-sidebar_btn">
-             {collapsed ? (
-              <MenuItem
-                icon={<MdOutlineKeyboardArrowRight/>}
-                onClick={handleCollapsedChange}
-              ></MenuItem>
-            ) : (
-              <MenuItem
-                suffix={<MdOutlineKeyboardArrowLeft/>}
-                onClick={handleCollapsedChange}
-              >
-                <div
-                  style={{
-                    padding: "9px",
-                    // textTransform: "uppercase",
-                    fontWeight: "bold",
-                    fontSize: 14,
-                    letterSpacing: "1px"
-                  }}
-                >
-                </div>
-              </MenuItem>
-            )}
-          
-          </Menu>
-        
-              
+        <Menu
+          onClick={() => collapseSidebar()}
+          className="collapse-sidebar_btn">
+          {collapsed ? (
+            <MenuItem
+              icon={<MdOutlineKeyboardArrowRight />}
+              onClick={handleCollapsedChange}></MenuItem>
+          ) : (
+            <MenuItem
+              suffix={<MdOutlineKeyboardArrowLeft />}
+              onClick={handleCollapsedChange}>
+              <div
+                style={{
+                  padding: '9px',
+                  // textTransform: "uppercase",
+                  fontWeight: 'bold',
+                  fontSize: 14,
+                  letterSpacing: '1px',
+                }}></div>
+            </MenuItem>
+          )}
+        </Menu>
       </main>
       <Menu>
         <MenuItem>
           {/* <img src={terroristprofilling} className="terrorist-profiling_logo" alt="terrorist profilling logo" />
           Terrorist Profiling */}
-<img src={terroristprofilling} className="terrorist-profiling_logo" alt="terrorist profilling logo" />
+          <img
+            src={terroristprofilling}
+            className="terrorist-profiling_logo"
+            alt="terrorist profilling logo"
+          />
           Terrorist Profiling
         </MenuItem>
-        <SubMenu src={network} icon={<BsGlobe2/>} label="Network Info">
+        <SubMenu src={network} icon={<BsGlobe2 />} label="Network Info">
           <MenuItem onClick={fetchTORDomainInfo}>TOR Domain</MenuItem>
           <MenuItem>TOR URL</MenuItem>
           <MenuItem>I2P Domain</MenuItem>
@@ -143,29 +145,25 @@ const Sidenav = (props) => {
           <MenuItem onClick={fetchUrl}>URL</MenuItem>
           <MenuItem onClick={fetchIP}>IP</MenuItem>
         </SubMenu>
-        <SubMenu  label="File Info" icon={<VscFile/>}>
+        <SubMenu label="File Info" icon={<VscFile />}>
           <MenuItem>Image</MenuItem>
           <MenuItem>Document</MenuItem>
           <MenuItem onClick={fetchFiles}>Other File</MenuItem>
         </SubMenu>
-        <SubMenu icon={<IoDocumentLockOutline/>} label="Leaked Info">
+        <SubMenu icon={<IoDocumentLockOutline />} label="Leaked Info">
           <MenuItem> Leaked Email </MenuItem>
           <MenuItem> Leaked Document </MenuItem>
         </SubMenu>
-        <SubMenu icon={<BiMessageRoundedDots/>} label="SNS Info">
+        <SubMenu icon={<BiMessageRoundedDots />} label="SNS Info">
           <MenuItem> Instagram </MenuItem>
           <MenuItem> Facebook </MenuItem>
         </SubMenu>
-        <SubMenu icon={<GrGroup/>} label="External Search Info">
+        <SubMenu icon={<GrGroup />} label="External Search Info">
           <MenuItem> Google Search </MenuItem>
         </SubMenu>
       </Menu>
-
-      
     </Sidebar>
   );
 };
 
 export default Sidenav;
-
-
