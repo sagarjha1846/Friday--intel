b56a9356355312fc46a9263bb5af9f96898e1f40
mayur <shelkemayur4700@gmail.com>
2023-03-18 10:47:03 +0530

    add activity section

--- src/pages/NewCase.js
+++ src/pages/NewCase.js
@@ -28,13 +28,16 @@ import DrawerInfo from '../components/DrawerInfo';
 import constants from '../constant/routesConstant';
 import {AiOutlineMail} from "react-icons/ai"
 import {AiOutlineLogout} from "react-icons/ai"
+import {AiOutlineDelete} from "react-icons/ai"
 import { BiBuildings } from "react-icons/bi";
 import { BiPhoneCall } from "react-icons/bi";
 
 
 const NewCase = () => {
   const {ROUTES} = constants;
+  const [mode, setMode] = useState(true);
   const [isOpen, setIsOpen] = useState(false);
+  const [isactiv, setIsactiv] = useState(false);
   const [isshow, setIsshow] = useState(false);
   // const [isshowDiv, setIsshowDiv] = useState(true);
   const [isHovering, setIsHovering] = useState(false);
@@ -88,6 +91,32 @@ const NewCase = () => {
     setIsHovering(false);
   };
   
+  const activity = () =>{
+    setIsactiv(!isactiv);
+  }
+
+  function themeChange(event) {
+    setMode(!mode);
+
+    const htmlElement = document.querySelector("html");
+    // set theme button background
+    const label = document.querySelector("#theme-label");
+    const PRIMARY = getComputedStyle(htmlElement).getPropertyValue("--primary-color");
+    const SECONDARY = getComputedStyle(htmlElement).getPropertyValue("--secondary-color");
+
+    htmlElement.style.setProperty("--primary-color", SECONDARY);
+    htmlElement.style.setProperty("--primary-color-1", SECONDARY);
+    htmlElement.style.setProperty("--secondary-color", PRIMARY);
+
+    label.style.backgroundSize = "cover";
+    // const logotheme = document.getElementsByClassName(".inactive")
+    // // eslint-disable-next-line no-unused-expressions
+    // logotheme.classList.remove("inactive")
+    
+
+
+
+}
 
   return (
     <>
@@ -142,12 +171,20 @@ const NewCase = () => {
             />
 
           </div> 
-          <button className="activity-icon btn-icon">         
+          <button className="activity-icon btn-icon" onClick={activity}>         
           <svg stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 24 24" height="30px" width="30px" xmlns="http://www.w3.org/2000/svg"><path d="M12 8v5h5v-2h-3V8z"></path><path d="M21.292 8.497a8.957 8.957 0 0 0-1.928-2.862 9.004 9.004 0 0 0-4.55-2.452 9.09 9.09 0 0 0-3.626 0 8.965 8.965 0 0 0-4.552 2.453 9.048 9.048 0 0 0-1.928 2.86A8.963 8.963 0 0 0 4 12l.001.025H2L5 16l3-3.975H6.001L6 12a6.957 6.957 0 0 1 1.195-3.913 7.066 7.066 0 0 1 1.891-1.892 7.034 7.034 0 0 1 2.503-1.054 7.003 7.003 0 0 1 8.269 5.445 7.117 7.117 0 0 1 0 2.824 6.936 6.936 0 0 1-1.054 2.503c-.25.371-.537.72-.854 1.036a7.058 7.058 0 0 1-2.225 1.501 6.98 6.98 0 0 1-1.313.408 7.117 7.117 0 0 1-2.823 0 6.957 6.957 0 0 1-2.501-1.053 7.066 7.066 0 0 1-1.037-.855l-1.414 1.414A8.985 8.985 0 0 0 13 21a9.05 9.05 0 0 0 3.503-.707 9.009 9.009 0 0 0 3.959-3.26A8.968 8.968 0 0 0 22 12a8.928 8.928 0 0 0-.708-3.503z"></path></svg>
-          
-                </button>  
+          </button>  
+          {isactiv && ( 
+            <div className="active">
+              <div className="active-data">
+                <h2>Activity</h2>
+              </div>
+
+              <div className="tri"></div>
+            </div>
+          )}
           <div>
-          <button className='btn-icon'>
+          <button className='btn-icon' onClick={themeChange}>
          <img src={sun} alt="sun" />
 </button>
 
@@ -159,7 +196,13 @@ const NewCase = () => {
           {/* NOTIFICATION BUTTON DATA---------------------------------------------------------------- */}
           {isshow && (
             <span className="noti">
-
+<span className="noti-data">
+  <h2>Notification</h2>
+  <article>You have a bug that needs to......</article>
+  <article>You have  bug that needs to.......</article>
+  <article>Welcome to friday intel</article>
+  <h4><span className="noti-icon"><AiOutlineDelete/></span>Delete Notification</h4>
+</span>
                 <div className="shape"></div>
               </span>
           )}
