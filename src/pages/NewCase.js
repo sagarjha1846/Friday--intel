46eb367fa9b326d14bd85f733642709ced6584c7
Vinit <vinit224488@gmail.com>
2023-04-28 20:56:54 +0530

    fix svg color for dark mode

--- src/pages/NewCase.js
+++ src/pages/NewCase.js
@@ -172,7 +172,7 @@ const NewCase = () => {
             {isOpen && <DrawerInfo />}
           </div>
           <form onSubmit={handleSubmit} className="searchbar-box">
-            <img src={fridaySearch} alt="star" className="p-3.5" />
+            <img src={fridaySearch} alt="star" className="p-3.5 search-logo" />
             <input
               type="text"
               className="search-bar-NC"
