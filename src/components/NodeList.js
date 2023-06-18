76642815d31c04d22b5edec04faa9b7dccdfb5ae
Sagar Jha <sagarj.ext@shapoorji.com>
2023-06-19 00:47:08 +0530

    Adding new menu and save node feature and loading previous data

--- src/components/NodeList.js
+++ src/components/NodeList.js
@@ -1,11 +1,10 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { AiFillPlusSquare } from 'react-icons/ai';
 import { ImCheckboxChecked } from 'react-icons/im';
-import { Menu } from 'react-pro-sidebar';
 import { MarkerType } from 'reactflow';
 
 import '../css/nodeList.css';
-import Paginate from './Paginate';
+import { Pagination } from 'antd';
 
 const NodeList = (props) => {
   const {
@@ -19,14 +18,10 @@ const NodeList = (props) => {
   } = props;
 
   const [currentPage, setCurrentPage] = useState(1);
-  const [postsPerPage] = useState(10);
+  const [postsPerPage] = useState(20);
 
   const indexOfLastPost = currentPage * postsPerPage;
   const indexOfFirstPost = indexOfLastPost - postsPerPage;
-
-  const paginate = (pageNumber) => {
-    setCurrentPage(pageNumber);
-  };
 
   const setNodeInCanvas = (data) => {
     const targetId = data;
@@ -67,56 +62,82 @@ const NodeList = (props) => {
     }
   };
 
-  const previousPage = () => {
-    if (currentPage !== 1) {
-      setCurrentPage(currentPage - 1);
+  const slicedData =
+    nodeList.query !== ''
+      ? activeMenu && nodeList.data[activeMenu] != null
+        ? nodeList.data[activeMenu]?.slice(
+            indexOfFirstPost,
+            indexOfFirstPost + postsPerPage,
+          )
+        : []
+      : [];
+
+  const itemRender = (_, type) => {
+    if (type === 'prev') {
+      return <span>Previous</span>;
+    }
+    if (type === 'next') {
+      return <span>Next</span>;
     }
   };
 
-  const nextPage = () => {
-    if (
-      currentPage !==
-      Math.ceil(nodeList.data[activeMenu]?.length / postsPerPage)
-    ) {
-      setCurrentPage(currentPage + 1);
+  useEffect(() => {
+    if (activeMenu) {
+      setCurrentPage(1);
     }
+  }, [activeMenu]);
+
+  const handlePagination = (e) => {
+    setCurrentPage(e);
   };
 
-  const slicedData = nodeList.data[activeMenu]?.slice(
-    indexOfFirstPost,
-    indexOfFirstPost + postsPerPage,
-  );
   return (
-    <Menu className="w-full">
-      {slicedData &&
-        slicedData?.map((node, index) => (
-          <div className="node-list">
-            <ul className="listItem p-1" key={index}>
-              <li className="listName line-clamp-2">{node || '-'}</li>
-              {isChecked[node] ? (
-                <ImCheckboxChecked
-                  size={'25px'}
-                  onClick={() => setNodeInCanvas(node)}
-                />
-              ) : (
-                <AiFillPlusSquare
-                  size={'25px'}
-                  onClick={() => setNodeInCanvas(node)}
-                />
-              )}
-            </ul>
-          </div>
-        ))}
-      <div className="grid place-content-center ">
-        <Paginate
-          postsPerPage={postsPerPage}
-          previousPage={previousPage}
-          nextPage={nextPage}
-          totalPosts={nodeList.data[activeMenu]?.length}
-          paginate={paginate}
-        />
+    <div className="overflow-hidden w-[256px] flex flex-col">
+      <div className="w-full ">
+        <div className="flex w-full flex-col">
+          {slicedData &&
+            slicedData.map((el) => (
+              <div className="flex justify-between p-2 ">
+                <div className="w-[80%] break-all text-left">{el}</div>
+                <div className=" cursor-pointer">
+                  {isChecked[el] ? (
+                    <ImCheckboxChecked
+                      size={'25px'}
+                      onClick={() => setNodeInCanvas(el)}
+                    />
+                  ) : (
+                    <AiFillPlusSquare
+                      size={'25px'}
+                      onClick={() => setNodeInCanvas(el)}
+                    />
+                  )}
+                </div>
+              </div>
+            ))}
+        </div>
       </div>
-    </Menu>
+
+      <div className="grid place-content-center ">
+        {activeMenu && nodeList.query !== '' && nodeList.data[activeMenu] ? (
+          <Pagination
+            simple
+            current={currentPage}
+            total={
+              activeMenu && nodeList.data[activeMenu]
+                ? nodeList.data[activeMenu].length
+                : 0
+            }
+            itemRender={itemRender}
+            defaultPageSize={postsPerPage}
+            showSizeChanger={false}
+            showQuickJumper={false}
+            onChange={handlePagination}
+          />
+        ) : (
+          'No Data To View'
+        )}
+      </div>
+    </div>
   );
 };
 
