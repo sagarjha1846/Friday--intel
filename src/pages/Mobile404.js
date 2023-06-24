46d9ee56df770577a80e9568c52209b8c88b93d6
Vinit <vinit224488@gmail.com>
2023-06-24 18:04:05 +0530

    add popup

--- src/pages/Mobile404.js
+++ src/pages/Mobile404.js
@@ -4,7 +4,7 @@ import '../css/mobile404.css';
 
 const Mobile404 = () => {
   return (
-    <div className="main">
+    <div className="main-mobile-404">
       <img src={Mobileerror} alt="logo" />
       <p className="paragraph">This window is not compatible with</p>
       <p className="device">mobile devices</p>
